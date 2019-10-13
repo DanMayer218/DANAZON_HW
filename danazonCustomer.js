7c43b0d2bdc5dfddd7b9c497b9a2785461c0162f
Daniel Mayer <danmayercraigs@gmail.com>
2019-10-12 23:46:27 -0400

    Commit, commit, commit.  So I set up the server connection in my danazonCustomer.js file.  And have successfully tested the connection, I also made a function to display the entire list of items with keys and values from the database and with my own personal message. looking good so far.

--- danazonCustomer.js
+++ danazonCustomer.js
@@ -1,2 +1,26 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "M@dm@x19",
+    database: "danazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+});
+
+function afterConnection() {
+    console.log("Time to loosen up the grip on your debit card, just look at this list of magical items for sale...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+  }
