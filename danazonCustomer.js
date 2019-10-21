eab73a30e1f378295c78f8530a0ff49ce6e27d51
Dans Bootcamp <dansbootcamp@AmbersImac.home>
2019-10-21 01:15:36 -0400

    Think I have it basically running, have to look at the database to see if stock quantitys are changing. but I need to sleep

--- danazonCustomer.js
+++ danazonCustomer.js
@@ -1,8 +1,8 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-var table = require('cli-table');
+// var table = require('cli-table');
 var colors = require("colors");
-var bodyParser = ("body-parser");
+// var bodyParser = ("body-parser");
 
 
 var connection = mysql.createConnection({
@@ -18,8 +18,8 @@ connection.connect(function(err) {
     // console.log("connected as id " + connection.threadId);
 });
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// var jsonParser = bodyParser.json();
 // function afterConnection() {
 //     console.log("Time to loosen up the grip on your debit card, just look at this list of magical items for sale...\n");
 //     connection.query("SELECT * FROM products", function(err, res) {
@@ -29,13 +29,107 @@ app.use(bodyParser.urlencoded({ extended: false }))
           console.log("Time to loosen up the grip on your debit card, just look at this list of magical items for sale...\n");
           connection.query("SELECT * FROM products", function(err, res) {
             if (err) throw err;
-            // Log all results of the SELECT statement
-            // console.log(res);
+
+            console.log("Welcome to Danazon home of the Absurd");
+            console.log("See anything that you like?");
+
+            for(var i = 0; i < res.length; i++){
+              console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
+              console.log('--------------------------------------------------------------------------------------------------')
+            }
+          
+            console.log(' ');
+           
+            inquirer.prompt([
+              {
+                type: "input",
+                name: "id",
+                message: "Enter the Id of the product you wish to waste your money on?",
+                validate: function(value){
+                  if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
+                    return true;
+                  } else{
+                    return false;
+                  }
+                }
+              },
+
+              {
+                type: "input",
+                name: "qty",
+                message: "How many of them do you want to buy today?",
+                validate: function(value){
+                  if(isNaN(value)){
+                    return false;
+                  } else{
+                    return true;
+                  }
+                }
+              }
+
+            ]).then(function(ans){
+              var whatToBuy = (ans.id)-1;
+              var howMuchToBuy = parseInt(ans.qty);
+              var grandTotal = parseFloat(((res[whatToBuy].Price)*howMuchToBuy).toFixed(2));
+        
+              //check if quantity is sufficient
+              if(res[whatToBuy].stock_quantity >= howMuchToBuy){
+                //after purchase, updates quantity in Products
+                connection.query("UPDATE Products SET ? WHERE ?", [
+                {stock_quantity: (res[whatToBuy].stock_quantity - howMuchToBuy)},
+                {ItemID: ans.id}
+                ], function(err, result){
+                    if(err) throw err;
+                    console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you in 3-5 business days.");
+                });
+        
+                connection.query("SELECT * FROM Departments", function(err, deptRes){
+                  if(err) throw err;
+                  var index;
+                  for(var i = 0; i < deptRes.length; i++){
+                    if(deptRes[i].department_name === res[whatToBuy].department_name){
+                      index = i;
+                    }
+                  }
+                  
+                  //updates totalSales in departments table
+                  connection.query("UPDATE Departments SET ? WHERE ?", [
+                  {TotalSales: deptRes[index].TotalSales + grandTotal},
+                  {department_name: res[whatToBuy].department_name}
+                  ], function(err, deptRes){
+                      if(err) throw err;
+                      //console.log("Updated Dept Sales.");
+                  });
+                });
+        
+              } else{
+                console.log("Sorry, there's not enough in stock!");
+              }
+        
+              reprompt();
+            })
+        })
+        }
+        
+        //asks if they would like to purchase another item
+        function reprompt(){
+          inquirer.prompt([{
+            type: "confirm",
+            name: "reply",
+            message: "Would you like to purchase another item?"
+          }]).then(function(ans){
+            if(ans.reply){
+              afterconnection();
+            } else{
+              console.log("See you soon!");
+            }
+          });
+        }
     
     
-      console.log(res.json);
-    });
-  };
+   
+
+  afterconnection();
 //       // Log all results of the SELECT statement
 //       var table = new Table({ 
 //         head: [colors.cyan('item_id'), colors.cyan('product_name'), colors.cyan('department_name'), colors.cyan('price'), colors.cyan('stock_quantity')],
