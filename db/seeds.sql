37c94dc481cc5eb024c59a516d0d6ea8ad6ac1a5
Daniel Mayer <danmayercraigs@gmail.com>
2019-11-12 08:21:20 -0500

    updated the products in the seeds.sql file now it is all more pedestrian

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+
+
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ('Cloak_of_mediocrity', 'household', 125.50, 8);
 
@@ -14,16 +16,47 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ('Bag_of_Random_Socks', 'mens_clothing', 20.90, 40);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Hennessey_Williams', 'liquor', 55.80, 20);
+VALUES ('Next_level_Turtleneck', 'Self-Esteem', 55.80, 20);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ('Toe_knife', 'outdoors', 15.75, 27);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Fathers_Love', 'mythical', 3000.00, 1);
+VALUES ('Last_Year_Back', 'mythical', 3000.00, 1);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('Gender_fluids', 'liquor', 75.00, 55);
+VALUES ('The_true_Narcissists_Shave_Kit', 'Mens_Hygiene', 75.00, 55);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ('4_skins_condoms', 'health', 9.00, 18);
+VALUES ('Daniel_Mayers_Greatest_hits', 'fictional_music', 9.00, 18);
+
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ('Cloak_of_mediocrity', 'household', 125.50, 8);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ('rotary_phone', 'electronics', 78.30, 10);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ('Universal_Self_Destruct_Button', 'office', 90.00, 12);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ('Starving_Artist_Joke_Book_and_Manual', 'books', 30.00, 9);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ('Bag_of_Random_Socks', 'mens_clothing', 20.90, 40);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ('Hennessey_Williams', 'liquor', 55.80, 20);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ('Toe_knife', 'outdoors', 15.75, 27);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ('Fathers_Love', 'mythical', 3000.00, 1);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ('Gender_fluids', 'liquor', 75.00, 55);
+
+-- INSERT INTO products (product_name, department_name, price, stock_quantity)
+-- VALUES ('4_skins_condoms', 'health', 9.00, 18);
